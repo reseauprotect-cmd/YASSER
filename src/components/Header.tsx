d461186317dc2bd18234dc0d0fb4f222e6e1f31f
itsolutions690-crypto <itsolutions690@gmail.com>
2025-10-28 11:18:24 +0100

    Updated UserInfoPage.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -9,9 +9,9 @@ export default function Header() {
       <div className="max-w-7xl mx-auto flex items-center justify-between px-6 py-4">
         <div className="flex items-center">
           <img
-            src="/image copy.png"
+            src="/image.png"
             alt="Allali"
-            className="h-10 w-auto"
+            className="h-12 w-auto"
           />
         </div>
 
