526fbebc79b487471d7c4bee4e1c6c3654256ee1
sysnova05-glitch <sysnova05@gmail.com>
2025-10-28 11:03:04 +0100

    Updated LoanCalculatorPage.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -9,9 +9,9 @@ export default function Header() {
       <div className="max-w-7xl mx-auto flex items-center justify-between px-6 py-4">
         <div className="flex items-center">
           <img
-            src="/image copy.png"
+            src="/image copy copy.png"
             alt="Allali"
-            className="h-10 w-auto"
+            className="h-8 w-auto"
           />
         </div>
 
